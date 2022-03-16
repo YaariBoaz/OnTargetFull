fb131c41568524389a38c1b81a91531ffda5ec8c
Boaz Yaari <boazyaarii@gmail.com>
2022-03-16 15:03:59 +0200

    added iphone plugins

---
+++ ios/capacitor-cordova-ios-plugins/sourcesstatic/CordovaPluginPurchases/PurchasesPlugin.swift
@@ -0,0 +1,11 @@
+import Cordova
+//
+//  PurchasesPlugin.swift
+//  HelloCordova
+//
+//  Created by Andrés Boedo on 9/23/20.
+//
+// This file is left intentionally blank,
+// the code for the plugin lives in CDVPurchasesPlugin
+
+import Foundation
