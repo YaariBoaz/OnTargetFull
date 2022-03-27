24e04065c1f7a2a877b8144b9dcd13f45b6f67c7
BellatrixTechnologies <@bellatrixtechnologies>
2022-03-27 13:46:11 +0500

    Final Code
    Change in in-app-products ids. and other fixes.

--- src/app/shared/services/purchase.service.ts
+++ src/app/shared/services/purchase.service.ts
@@ -6,11 +6,17 @@ import { Purchases } from '@awesome-cordova-plugins/purchases/ngx';
 import { Resolve } from '@angular/router';
 import { ApiService } from './api.service';
 import { UserService } from './user.service';
-import {Subject} from 'rxjs';
+import { Subject } from 'rxjs';
 
-const ADL_IAP_KEY = 'adl';
+const ADL_IAP_KEY = 'autoRenew';
 const ADL_IAP_KEY_2_SESSIONS = 'twoSessionSub';
 const ADL_IAP_KEY_6_SESSIONS = 'sixSessions';
+
+
+const ADL_IAP_KEY_md = 'adl_montly';
+const ADL_IAP_KEY_2_SESSIONS_md = 'two_session_sub';
+const ADL_IAP_KEY_6_SESSIONS_md = 'six_sessions';
+
 
 @Injectable({
     providedIn: 'root'
@@ -21,14 +27,14 @@ export class PurchaseService {
     $notifyPurchaseApproved = new Subject();
     constructor(private purchases: Purchases,
         private plt: Platform,
-        private store: InAppPurchase2,
+        public store: InAppPurchase2,
         private srvApi: ApiService) {
         this.plt.ready().then(() => {
             this.purchases.setDebugLogsEnabled(true); // Enable to get debug logs
             this.purchases.setup('appl_zvLCiuPbRXLEBKOylPgsndRSFNX');
             this.purchases.getOfferings().then((offerings) => {
                 if (offerings.current !== null) {
-                     ;
+                    ;
                 }
             },
                 error => {
@@ -40,7 +46,7 @@ export class PurchaseService {
             this.store.verbosity = this.store.DEBUG;
 
             this.registerProducts();
-            this.setupListeners();
+
 
             // Get the real product information
 
@@ -55,28 +61,68 @@ export class PurchaseService {
         return new Promise((resolve, reject) => {
             this.store.ready(() => {
                 this.products = this.store.products;
+                console.log("this.products", JSON.stringify(this.products));
                 resolve(this.products);
-
             });
-        })
+        });
     }
 
     registerProducts() {
-        this.store.register({
-            id: ADL_IAP_KEY_6_SESSIONS,
-            type: this.store.CONSUMABLE,
+        if (this.plt.is('ios')) {
+            this.store.register({
+                id: ADL_IAP_KEY_6_SESSIONS,
+                type: this.store.NON_CONSUMABLE,
+            });
+
+            this.store.register({
+                id: ADL_IAP_KEY_2_SESSIONS,
+                type: this.store.NON_CONSUMABLE,
+            });
+
+            this.store.register({
+                id: ADL_IAP_KEY,
+                type: this.store.NON_CONSUMABLE,
+            });
+        } else {
+            this.store.register({
+                id: ADL_IAP_KEY_md,
+                type: this.store.NON_CONSUMABLE,
+            });
+
+            this.store.register({
+                id: ADL_IAP_KEY_6_SESSIONS_md,
+                type: this.store.CONSUMABLE,
+            });
+
+            this.store.register({
+                id: ADL_IAP_KEY_2_SESSIONS_md,
+                type: this.store.CONSUMABLE,
+            });
+
+        }
+
+
+        // Register event handlers for the specific product
+        // this.store.when('product').registered((product: IAPProduct) => {
+        //     console.log('Registered: ADL_IAP_KEY_md ' + JSON.stringify(product));
+        // });
+
+        // Track all store errors
+        this.store.error((err) => {
+            console.error('Store Error ' + JSON.stringify(err));
         });
 
-        this.store.register({
-            id: ADL_IAP_KEY_2_SESSIONS,
-            type: this.store.CONSUMABLE,
+        // Run some code only when the store is ready to be used
+        this.store.ready(() => {
+            console.log('Store is ready');
+            console.log('Products: ' + JSON.stringify(this.store.products));
+            console.log(JSON.stringify(this.store.get("my_product_id")));
         });
 
-        this.store.register({
-            id: ADL_IAP_KEY,
-            type: this.store.PAID_SUBSCRIPTION,
-        });
+
         this.store.refresh();
+        this.setupListeners();
+        // this.getProducts();
     }
 
     setupListeners() {
@@ -84,13 +130,13 @@ export class PurchaseService {
         this.store.when('product')
             .approved((p: IAPProduct) => {
                 if (p && !p.id.includes('.onTarget.app')) {
-                    console.log('i am in approved event', p);
+                    // console.log('i am in approved event', p);
                     this.srvApi.uploadSubscription(p);
                     this.$notifyPurchaseApproved.next(true);
                 }
             })
             .verified((p: IAPProduct) => {
-                console.log('i am in verified event', p);
+                // console.log('i am in verified event', p);
                 p.finish();
             }).cancelled((p) => {
                 console.log('i am in cancelled event', p);
@@ -109,10 +155,10 @@ export class PurchaseService {
         });
     }
 
-    // To comply with AppStore rules
-    restore() {
-        this.store.refresh();
-    }
+    // // To comply with AppStore rules
+    // restore() {
+    //     this.store.refresh();
+    // }
 
 
 
